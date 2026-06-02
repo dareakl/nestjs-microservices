94377e6d94d03e79200f12bb988186a1d51020bc
Sathish Raja <dareakl@users.noreply.github.com>
2026-06-02 11:29:19 +0530

    global error handler implemented for micro services

--- libs/rpc/src/index.ts
+++ libs/rpc/src/index.ts
@@ -1,2 +1,7 @@
 export * from './rpc.module';
-export * from './rpc.service';
+export * from './rpc.types';
+export * from './rpc.helpers';
+export * from './rpc-exception.filter';
+export * from './rpc.setup';
+
+export * from './http/rpc-error.mapper';
