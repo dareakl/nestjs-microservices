75001f07a79ed5b7c7ed589a6f7ead1ca535ea6f
Sathish Raja <dareakl@users.noreply.github.com>
2026-06-04 10:10:06 +0530

    cloudinary config, media schema, media dto created

---
+++ apps/media/src/media/media.dto.ts
@@ -0,0 +1,27 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class UploadProductImageDto {
+  @IsString()
+  fileName: string;
+
+  @IsString()
+  mimeType: string;
+
+  @IsString()
+  base64: string;
+
+  @IsString()
+  uploadByUserId: string;
+}
+
+export class AttachToProductDto {
+  @IsString()
+  mediaId: string;
+
+  @IsString()
+  productId: string;
+
+  @IsOptional()
+  @IsString()
+  attachedByUserId?: string;
+}
