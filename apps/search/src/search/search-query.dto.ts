7877e6ec02fa9af79b1eab1b28d8719ea49aa7bd
Sathish Raja <dareakl@users.noreply.github.com>
2026-06-07 22:59:43 +0530

    product event dto, search query dto created

---
+++ apps/search/src/search/search-query.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class SearchQueryDto {
+  @IsString()
+  q: string;
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  limit?: Number;
+}
